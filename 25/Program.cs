84fb11c20290248cf6cbf04775eba57e75c48496
avkirilin <nerosashok@mail.ru>
2023-01-15 17:49:04 +0300

    Переименование переменных в удобоваримый формат

--- 25/Program.cs
+++ 25/Program.cs
@@ -1,14 +1,14 @@
 ﻿//Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
 //3, 5 -> 243 (3⁵)
 //2, 4 -> 16
-double Stepen (int first_num, int second_num)
+double Stepen (int firstNum, int secondNum)
 {
-    double result = Math.Pow(first_num, second_num);
+    double result = Math.Pow(firstNum, secondNum);
     return result;
 }
 Console.WriteLine ("Введите первое число");
-int first_num = Convert.ToInt32(Console.ReadLine());
+int firstNum = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine ("Введите второе число");
-int second_num = Convert.ToInt32(Console.ReadLine());
-double stepen_chisla = Stepen(first_num, second_num);
-Console.WriteLine ($"{first_num} в степени {second_num} будет равно {stepen_chisla}");
+int secondNum = Convert.ToInt32(Console.ReadLine());
+double stepenChisla = Stepen(firstNum, secondNum);
+Console.WriteLine ($"{firstNum} в степени {secondNum} будет равно {stepenChisla}");
