0517177fdff2180583410f97599a9cd9286d06d7
avkirilin <nerosashok@mail.ru>
2023-01-15 22:51:50 +0300

    Решена третья задача домашнего задания

---
+++ 29/Program.cs
@@ -0,0 +1,24 @@
+﻿//Напишите программу, которая задает массивиз 8 элементови выводит их на экран
+void CreateArray(int[] newArray)
+{
+    int lenght = newArray.Length;
+    int index = 0;
+    while (index < lenght)
+    {
+        newArray[index] = new Random().Next(1, 100);
+        index++;
+    }
+}
+void ShowArray(int[] anotherArray)
+{
+    int count = anotherArray.Length;
+    int place = 0;
+    while (place < count)
+    {
+        Console.Write($"{anotherArray[place]} ");
+        place++;
+    }
+}
+int[] array = new int[8];
+CreateArray(array);
+ShowArray(array);
