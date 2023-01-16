3b08813fa317202d3ac121a75861520d2fddc657
avkirilin <nerosashok@mail.ru>
2023-01-16 13:27:38 +0300

    Решение 27 задачи 4 семинара

---
+++ 27/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+Console.Clear();
+int Summa(string str)
+{
+    int lenghtArray = str.Length;
+    int sum = 0;
+    for (int i = 0; i < lenghtArray; i++)
+    {
+        //Console.WriteLine($"{(int)Char.GetNumericValue(newString[i])} {sum}");
+        sum = sum + (int)Char.GetNumericValue(str[i]);
+    }
+    return sum;
+}
+Console.WriteLine();
+void ShowSumDigitsOfNumber(int number)
+    {
+        int result = 0;
+        int tempNumber = number;
+        while (tempNumber != 0)
+        {
+            result += tempNumber % 10; //result = result + tempNumber % 10
+            tempNumber /= 10;          //tempNumber = tempNumber / 10
+        }
+        Console.WriteLine($"{number} -> {result}");
+    }
+
+Console.WriteLine("Введите любое целое число");
+string newString = Console.ReadLine()!;
+int number = Convert.ToInt32(newString);
+// int chislo = Convert.ToInt32(newString);
+Console.WriteLine($"Сумма всех чисел из заданного числа будет равна: {Summa(newString)}");
+ShowSumDigitsOfNumber(number);
